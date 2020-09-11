c5699102af6111071589be1d90f9a0cd45897bc6
hoochicken <mareike.riegel@ql.de>
2020-09-11 15:15:52 +0200

    Demo route added

--- dungeon-server/src/Controller/DemoController.php
+++ dungeon-server/src/Controller/DemoController.php
@@ -2,13 +2,9 @@
 namespace App\Controller;
 
 use Symfony\Component\HttpFoundation\JsonResponse;
-use Symfony\Component\Routing\Annotation\Route;
 
 class DemoController extends ApiController
 {
-    /**
-     * @Route("/demo")
-     */
     public function demonicAction()
     {
         return $this->respond([
