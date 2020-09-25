0c74f20156fb97e7b92347008bf46d020598fc38
hoochicken <mareike.riegel@ql.de>
2020-09-25 12:46:52 +0200

    client: use router

---
+++ dungeon-client/src/components/demo/Demonic.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <h2>Demonic</h2>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Demonic"
+    }
+</script>
+
+<style scoped>
+
+</style>
--- dungeon-client/src/main.js
+++ dungeon-client/src/main.js
@@ -1,13 +1,18 @@
 import Vue from 'vue'
 import App from './App.vue'
-import axios from './plugins/axios'
 import VueRouter from 'vue-router'
+// import axios from './plugins/axios'
+
+import Demonic from './components/demo/Demonic';
 
 Vue.config.productionTip = false;
-Vue.use(VueRouter, axios);
-Vue.axios.defaults.baseURL = `http://${process.env.VUE_APP_ENV_HOST}:${process.env.VUE_APP_ENV_PORT}`;
+Vue.use(VueRouter);
+// Vue.use(VueRouter, axios);
+// Vue.axios.defaults.baseURL = `http://${process.env.VUE_APP_ENV_HOST}:${process.env.VUE_APP_ENV_PORT}`;
+// Vue.axios.defaults.baseURL = `http://localhost:8000`;
 
 const routes = [
+  { path: '/demo', component: Demonic },
   { path: '/', component: App }
 ];
 
@@ -15,6 +20,9 @@ const router = new VueRouter({
   mode: 'history',
   routes
 });
+
+console.log(Demonic);
+
 
 new Vue({
   router,
