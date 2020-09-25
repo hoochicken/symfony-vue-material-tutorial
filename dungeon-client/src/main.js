80c001968bf8eefec4cc97b223b15428f5ef43b6
hoochicken <mareike.riegel@ql.de>
2020-09-25 12:17:21 +0200

    client: exios and router added

--- dungeon-client/src/main.js
+++ dungeon-client/src/main.js
@@ -1,8 +1,22 @@
 import Vue from 'vue'
 import App from './App.vue'
+import axios from './plugins/axios'
+import VueRouter from 'vue-router'
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+Vue.use(VueRouter, axios);
+Vue.axios.defaults.baseURL = `http://${process.env.VUE_APP_ENV_HOST}:${process.env.VUE_APP_ENV_PORT}`;
+
+const routes = [
+  { path: '/', component: App }
+];
+
+const router = new VueRouter({
+  mode: 'history',
+  routes
+});
 
 new Vue({
+  router,
   render: h => h(App),
-}).$mount('#app')
+}).$mount('#app');
